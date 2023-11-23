36348dfdbcecaccccfac418d86016cec7862df7b
artyomakulov <artyomakulov@gmail.com>
2023-11-23 21:54:39 +0200

    add contacts and fixes

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import css from "./Works.module.css";
 
 const Works = () => {
   return (
-    <div>Works</div>
-  )
-}
+    <div className={css.container}>
+      <h2 className={css.title}>Works</h2>
+      <div className={css.works_block}>
+        <div className={css.item_container}></div>
+        <div className={css.item_container}></div>
+        <div className={css.item_container}></div>
+        <div className={css.item_container}></div>
+      </div>
+    </div>
+  );
+};
 
-export default Works
+export default Works;
