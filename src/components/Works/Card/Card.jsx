ae1d706cc96a6aa2fd4764cb7306081ca7bb44a0
artyomakulov <artyomakulov@gmail.com>
2023-11-27 15:43:57 +0200

    add works/fixes

--- src/components/Works/Card/Card.jsx
+++ src/components/Works/Card/Card.jsx
@@ -2,23 +2,23 @@ import css from "./Card.module.css";
 
 import React from "react";
 
-const Card = ({ work }) => {
+const Card = (props) => {
   return (
     <div className={css.card}>
       <div className={css.card_text_block}>
-        <h5 className={css.card_title}>{work.title}</h5>
-        <p className={css.card_tech}>{work.stack}</p>
-        <p className={css.card_role}>{work.role}</p>
-        <p className={css.card_disc}>{work.discription}</p>
-        <a href={work.link}>
+        <h5 className={css.card_title}>{props.title}</h5>
+        <p className={css.card_tech}>{props.stack}</p>
+        <p className={css.card_role}>{props.role}</p>
+        <p className={css.card_disc}>{props.discription}</p>
+        <a href={props.link}>
           <button className={css.button}>Open project</button>
         </a>
       </div>
       <div className={css.image_block}>
         <img
           className={css.img}
-          src={work.image}
-          alt={work.title}
+          src={props.image}
+          alt={props.title}
           width={200}
         />
       </div>
--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,20 +1,54 @@
+// Works.js
+
 import React from "react";
 import css from "./Works.module.css";
 import Card from "./Card/Card";
 import { works } from "../../data/works";
 
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 1440 },
+    items: 3,
+  },
+  desktop: {
+    breakpoint: { max: 1440, min: 768 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 768, min: 320 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 320, min: 0 },
+    items: 1,
+  },
+};
+
 const Works = () => {
+  const work = works.map((item) => (
+    <Card
+      title={item.title}
+      stack={item.stack}
+      role={item.role}
+      discription={item.discription}
+      link={item.link}
+      image={item.image}
+    />
+  ));
+
   return (
     <div className={css.container}>
       <h2 className={css.title}>Works</h2>
-      <div className={css.works_block}>
-        {works.map((work) => (
-
-          <div key={work.id}>
-            <Card work={work} />
-          </div>
-        ))}
-      </div>
+      <Carousel
+        responsive={responsive}
+        showDots={true}
+        infinite={true}
+        containerClass={css.carouselContainer}
+      >{work}
+      </Carousel>
     </div>
   );
 };
