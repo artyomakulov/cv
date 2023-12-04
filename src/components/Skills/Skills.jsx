2474fd480f8d061ee3179da682130c405831b70f
artyomakulov <artyomakulov@gmail.com>
2023-12-04 23:45:37 +0200

    add skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -5,42 +5,150 @@ import { ReactComponent as Cssicon } from "../../ulits/images/skills/css.svg";
 import { ReactComponent as Js } from "../../ulits/images/skills/js.svg";
 import { ReactComponent as Git } from "../../ulits/images/skills/git.svg";
 import { ReactComponent as Scss } from "../../ulits/images/skills/scss.svg";
-
+import { ReactComponent as Bootstrap } from "../../ulits/images/skills/bootstrap.svg";
+import { ReactComponent as Exporess } from "../../ulits/images/skills/express.svg";
+import { ReactComponent as Mongoose } from "../../ulits/images/skills/mongoose.svg";
+import { ReactComponent as Mui } from "../../ulits/images/skills/mui.svg";
+import { ReactComponent as Nodejs } from "../../ulits/images/skills/node-js.svg";
+import { ReactComponent as Redux } from "../../ulits/images/skills/redux.svg";
+import { ReactComponent as Reacticon } from "../../ulits/images/skills/react.svg";
+import { ReactComponent as Restapi } from "../../ulits/images/skills/rest-api.svg";
+import { ReactComponent as Socket } from "../../ulits/images/skills/socketio.svg";
+import { ReactComponent as Webpack } from "../../ulits/images/skills/webpack.svg";
+import { ReactComponent as Typescript } from "../../ulits/images/skills/typescript.svg";
 
 const Skills = () => {
   return (
     <div className={css.container}>
       <h2 className={css.title}>My Skills</h2>
-      <div className={css.skill_block}>
-        <ul className={css.list}>
-          <li className={css.list_item}>
+      <div className={css.gallery_container}>
+        <ul className={css.gallery_grid}>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_1}`}
+          >
+            <div className={`${css.item_container} ${css.bootstrap}`}>
+              <Bootstrap />
+              <p className={css.text}>Bootstrap</p>
+            </div>
+          </li>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_2}`}
+          >
+            <div className={`${css.item_container} ${css.express}`}>
+              <Exporess />
+              <p className={css.text}>Express.js</p>
+            </div>
+          </li>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_3}`}
+          >
+            <div className={`${css.item_container} ${css.mongoose}`}>
+              <Mongoose />
+              <p className={css.text}>Mongoose</p>
+            </div>
+          </li>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_4}`}
+          >
+            <div className={`${css.item_container} ${css.mui}`}>
+              <Mui />
+              <p className={css.text}>Material UI</p>
+            </div>
+          </li>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_5}`}
+          >
+            <div className={`${css.item_container_big} ${css.node}`}>
+              <Nodejs />
+              <p className={css.text}>Node.js</p>
+            </div>
+          </li>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_6}`}
+          >
+            <div className={`${css.item_container_big} ${css.react}`}>
+              <Reacticon />
+              <p className={css.text}>React</p>
+            </div>
+          </li>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_7}`}
+          >
+            <div className={`${css.item_container} ${css.redux}`}>
+              <Redux />
+              <p className={css.text}>Redux</p>
+            </div>
+          </li>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_8}`}
+          >
+            <div className={`${css.item_container} ${css.restapi}`}>
+              <Restapi />
+              <p className={css.text}>Rest API</p>
+            </div>
+          </li>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_9}`}
+          >
             <div className={`${css.item_container} ${css.html}`}>
               <Html />
               <p className={css.text}>HTML5</p>
             </div>
           </li>
-          <li className={css.list_item}>
-            <div className={`${css.item_container} ${css.css}`}>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_10}`}
+          >
+            <div className={`${css.item_container_big} ${css.css}`}>
               <Cssicon />
               <p className={css.text}>CSS3</p>
             </div>
           </li>
-          <li className={css.list_item}>
-            <div className={`${css.item_container} ${css.js}`}>
-              <Js/>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_11}`}
+          >
+            <div className={`${css.item_container_big} ${css.js}`}>
+              <Js />
               <p className={css.text}>JavaScript</p>
             </div>
           </li>
-          <li className={css.list_item}>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_12}`}
+          >
             <div className={`${css.item_container} ${css.git}`}>
-              <Git/>
+              <Git />
               <p className={css.text}>Git</p>
             </div>
           </li>
-          <li className={css.list_item}>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_13}`}
+          >
             <div className={`${css.item_container} ${css.scss}`}>
-             <Scss/>
+              <Scss />
               <p className={css.text}>SCSS</p>
+            </div>
+          </li>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_14}`}
+          >
+            <div className={`${css.item_container} ${css.socket}`}>
+              <Socket />
+              <p className={css.text}>Socket.io</p>
+            </div>
+          </li>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_15}`}
+          >
+            <div className={`${css.item_container} ${css.ts}`}>
+              <Typescript />
+              <p className={css.text}>TypeScript</p>
+            </div>
+          </li>
+          <li
+            className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_16}`}
+          >
+            <div className={`${css.item_container} ${css.webpack}`}>
+              <Webpack />
+              <p className={css.text}>Webpack</p>
             </div>
           </li>
         </ul>
