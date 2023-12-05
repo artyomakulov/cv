970ff2160d884fc71ffdb7d301f9ff3ac66447cf
artyomakulov <artyomakulov@gmail.com>
2023-12-05 16:21:47 +0200

    fix styles skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -30,143 +30,167 @@ const Skills = () => {
       </h2>
       <div className={css.gallery_container}>
         <div>
-          <ul className={css.gallery_grid} data-aos="zoom-in">
+          <ul className={css.gallery_grid}>
             <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_1}`}
+              className={`${css.list_item} ${css.gallery_grid_item_1}`}
               data-aos="fade-right"
             >
-              <div className={`${css.item_container} ${css.webpack}`}>
+              <div
+                className={`${css.item_container} ${css.gallery_grid_item} ${css.webpack}`}
+              >
                 <Webpack />
                 <p className={css.text}>Webpack</p>
               </div>
             </li>
-            <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_2}`}
-            >
-              <div className={`${css.item_container} ${css.socket}`}>
+            <li className={`${css.list_item} ${css.gallery_grid_item_2}`}>
+              <div
+                className={`${css.item_container} ${css.gallery_grid_item} ${css.socket}`}
+              >
                 <Socket />
                 <p className={css.text}>Socket.io</p>
               </div>
             </li>
             <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_3}`}
+              className={`${css.list_item} ${css.gallery_grid_item_3}`}
               data-aos="fade-up-right"
             >
-              <div className={`${css.item_container} ${css.mongoose}`}>
+              <div
+                className={`${css.item_container} ${css.gallery_grid_item} ${css.mongoose}`}
+              >
                 <Mongoose />
                 <p className={css.text}>Mongoose</p>
               </div>
             </li>
             <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_4}`}
+              className={`${css.list_item} ${css.gallery_grid_item_4}`}
               data-aos="fade-up-left"
             >
-              <div className={`${css.item_container} ${css.mui}`}>
+              <div
+                className={`${css.item_container} ${css.gallery_grid_item} ${css.mui}`}
+              >
                 <Mui />
                 <p className={css.text}>Material UI</p>
               </div>
             </li>
             <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_5}`}
+              className={`${css.list_item} ${css.gallery_grid_item_5}`}
               data-aos="fade-down-left"
             >
-              <div className={`${css.item_container_big} ${css.node}`}>
+              <div
+                className={`${css.item_container_big} ${css.gallery_grid_item} ${css.node}`}
+              >
                 <Nodejs />
                 <p className={css.text}>Node.js</p>
               </div>
             </li>
-            <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_6}`}
-            >
-              <div className={`${css.item_container_big} ${css.react}`}>
+            <li className={`${css.list_item} ${css.gallery_grid_item_6}`}>
+              <div
+                className={`${css.item_container_big} ${css.gallery_grid_item} ${css.react}`}
+              >
                 <Reacticon />
                 <p className={css.text}>React</p>
               </div>
             </li>
             <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_7}`}
+              className={`${css.list_item} ${css.gallery_grid_item_7}`}
               data-aos="fade-down-left"
             >
-              <div className={`${css.item_container} ${css.redux}`}>
+              <div
+                className={`${css.item_container} ${css.gallery_grid_item} ${css.redux}`}
+              >
                 <Redux />
                 <p className={css.text}>Redux</p>
               </div>
             </li>
             <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_8}`}
+              className={`${css.list_item} ${css.gallery_grid_item_8}`}
               data-aos="fade-up"
             >
-              <div className={`${css.item_container} ${css.restapi}`}>
+              <div
+                className={`${css.item_container} ${css.gallery_grid_item} ${css.restapi}`}
+              >
                 <Restapi />
                 <p className={css.text}>Rest API</p>
               </div>
             </li>
             <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_9}`}
+              className={`${css.list_item} ${css.gallery_grid_item_9}`}
               data-aos="fade-down"
             >
-              <div className={`${css.item_container} ${css.html}`}>
+              <div
+                className={`${css.item_container} ${css.gallery_grid_item} ${css.html}`}
+              >
                 <Html />
                 <p className={css.text}>HTML5</p>
               </div>
             </li>
             <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_10}`}
+              className={`${css.list_item} ${css.gallery_grid_item_10}`}
               data-aos="fade-up-right"
             >
-              <div className={`${css.item_container_big} ${css.css}`}>
+              <div
+                className={`${css.item_container_big} ${css.gallery_grid_item} ${css.css}`}
+              >
                 <Cssicon />
                 <p className={css.text}>CSS3</p>
               </div>
             </li>
             <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_11}`}
+              className={`${css.list_item} ${css.gallery_grid_item_11}`}
               data-aos="fade-down-right"
             >
-              <div className={`${css.item_container_big} ${css.js}`}>
+              <div
+                className={`${css.item_container_big} ${css.gallery_grid_item} ${css.js}`}
+              >
                 <Js />
                 <p className={css.text}>JavaScript</p>
               </div>
             </li>
             <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_12}`}
+              className={`${css.list_item} ${css.gallery_grid_item_12}`}
               data-aos="fade-left"
             >
-              <div className={`${css.item_container} ${css.git}`}>
+              <div
+                className={`${css.item_container} ${css.gallery_grid_item} ${css.git}`}
+              >
                 <Git />
                 <p className={css.text}>Git</p>
               </div>
             </li>
             <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_13}`}
+              className={`${css.list_item} ${css.gallery_grid_item_13}`}
               data-aos="fade-down-right"
             >
-              <div className={`${css.item_container} ${css.scss}`}>
+              <div
+                className={`${css.item_container} ${css.gallery_grid_item} ${css.scss}`}
+              >
                 <Scss />
                 <p className={css.text}>SCSS</p>
               </div>
             </li>
-            <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_14}`}
-            >
-              <div className={`${css.item_container} ${css.ts}`}>
+            <li className={`${css.list_item} ${css.gallery_grid_item_14}`}>
+              <div
+                className={`${css.item_container} ${css.gallery_grid_item} ${css.ts}`}
+              >
                 <Typescript />
                 <p className={css.text}>TypeScript</p>
               </div>
             </li>
-            <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_15}`}
-            >
-              <div className={`${css.item_container} ${css.express}`}>
+            <li className={`${css.list_item} ${css.gallery_grid_item_15}`}>
+              <div
+                className={`${css.item_container} ${css.gallery_grid_item} ${css.express}`}
+              >
                 <Exporess />
                 <p className={css.text}>Express.js</p>
               </div>
             </li>
             <li
-              className={`${css.list_item} ${css.gallery_grid_item} ${css.gallery_grid_item_16}`}
+              className={`${css.list_item} ${css.gallery_grid_item_16}`}
               data-aos="fade-up-left"
             >
-              <div className={`${css.item_container} ${css.bootstrap}`}>
+              <div
+                className={`${css.item_container} ${css.gallery_grid_item} ${css.bootstrap}`}
+              >
                 <Bootstrap />
                 <p className={css.text}>Bootstrap</p>
               </div>
