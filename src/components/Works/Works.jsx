61eb41ed23862edfd15b85df0f94eed4ff1fb396
artyomakulov <artyomakulov@gmail.com>
2023-12-06 19:34:49 +0200

    fix

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -13,18 +13,18 @@ const responsive = {
     breakpoint: { max: 4000, min: 1280 },
     items: 3,
   },
-  // desktop: {
-  //   breakpoint: { max: 1280, min: 768 },
-  //   items: 3,
-  // },
-  // tablet: {
-  //   breakpoint: { max: 768, min: 320 },
-  //   items: 3,
-  // },
-  // mobile: {
-  //   breakpoint: { max: 320, min: 0 },
-  //   items: 2,
-  // },
+  desktop: {
+    breakpoint: { max: 1280, min: 768 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 768, min: 320 },
+    items: 3,
+  },
+  mobile: {
+    breakpoint: { max: 320, min: 0 },
+    items: 2,
+  },
 };
 
 const Works = () => {
